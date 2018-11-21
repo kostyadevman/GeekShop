99fae7a414b062a5be2d207ee4cb71a47a6046be
jora <jora@email.ru>
2018-11-21 23:42:11 +0300

    Begin lesson_2

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,12 +1,65 @@
 from django.shortcuts import render
-
+import json
 
 def main(request):
-    return render(request, 'mainapp/index.html')
+    # title = 'Главная'
+    # products = [
+    #         {
+    #             "name": "Отличный стул",
+    #             "desc": "Расположитесь комфортно",
+    #             "image_src": "product-1.jpg",
+    #             "image_href": "/product/1/",
+    #             "alt": "продукт 1"
+    #         },
+    #         {
+    #             "name": "Стул повышенного качества",
+    #             "desc": "Не оторваться",
+    #             "image_src": "product-2.jpg",
+    #             "image_href": "/product/2/",
+    #             "alt": "продукт 2"
+    #         }
+    # ]
+    # content = {'title': title, 'products': products}
+    json_data = open('static/products.json').read()
+    content = json.loads(json_data)
+    return render(request, 'mainapp/index.html', content)
 
 
 def products(request):
-    return render(request, 'mainapp/products.html')
+    title = 'Продукты'
+    links_menu = [
+        {'href': 'products_all', 'name': 'все'},
+        {'href': 'products_home', 'name': 'дом'},
+        {'href': 'products_office', 'name': 'офис'},
+        {'href': 'products_modern', 'name': 'модерн'},
+        {'href': 'products_classic', 'name': 'классика'},
+    ]
+    same_products = [
+        {
+            'name': 'Отличный стул',
+            'desc': 'Не оторваться',
+            'image_src': 'product-11.jpg',
+            'alt': 'продукт 11'
+        },
+        {
+            'name': 'Стул повышенного качества',
+            'desc': 'Комфортно',
+            'image_src': 'product-21.jpg',
+            'alt': 'продукт 21'
+        },
+        {
+            'name': 'Стул премиального качества',
+            'desc': 'Просто попробуйте',
+            'image_src': 'product-31.jpg',
+            'alt': 'продукт 31'
+        }
+    ]
+    content = {
+        'title': title,
+        'links_menu': links_menu,
+        'same_products': same_products
+    }
+    return render(request, 'mainapp/products.html', content)
 
 
 def contact(request):

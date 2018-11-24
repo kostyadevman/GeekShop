2aede30fa520bc7739199ce36b892c76346ed71e
jora <jora@email.ru>
2018-11-24 18:26:55 +0300

    Initial commit from src lesson_3

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,32 +1,31 @@
 from django.shortcuts import render
-import json
+import datetime
+
 
 def main(request):
-    title = 'Главная'
-    # products = [
-    #         {
-    #             "name": "Отличный стул",
-    #             "desc": "Расположитесь комфортно",
-    #             "image_src": "product-1.jpg",
-    #             "image_href": "/product/1/",
-    #             "alt": "продукт 1"
-    #         },
-    #         {
-    #             "name": "Стул повышенного качества",
-    #             "desc": "Не оторваться",
-    #             "image_src": "product-2.jpg",
-    #             "image_href": "/product/2/",
-    #             "alt": "продукт 2"
-    #         },
-    # ]
-    # content = {'title': title, 'products': products}
-    json_data = open('static/products.json').read()
-    content = json.loads(json_data)
+    title = 'главная'
+    products = [
+        {
+            'name': 'Отличный стул',
+            'desc': 'Расположитесь комфортно.',
+            'image_src': 'product-1.jpg',
+            'image_href': '/product/1/',
+            'alt': 'продукт 1'
+        },
+        {
+            'name': 'Стул повышенного качества', 
+            'desc': 'Не оторваться.',
+            'image_src': 'product-2.jpg',
+            'image_href': '/product/2/',
+            'alt': 'продукт 2'
+        },
+    ]
+    content = {'title': title, 'products': products}
     return render(request, 'mainapp/index.html', content)
-
+    
 
 def products(request):
-    title = 'Продукты'
+    title = 'продукты'
     links_menu = [
         {'href': 'products_all', 'name': 'все'},
         {'href': 'products_home', 'name': 'дом'},
@@ -37,31 +36,51 @@ def products(request):
     same_products = [
         {
             'name': 'Отличный стул',
-            'desc': 'Не оторваться',
+            'desc': 'Не оторваться.',
             'image_src': 'product-11.jpg',
             'alt': 'продукт 11'
         },
         {
-            'name': 'Стул повышенного качества',
-            'desc': 'Комфортно',
+            'name': 'Стул повышенного качества', 
+            'desc': 'Комфортно.',
             'image_src': 'product-21.jpg',
             'alt': 'продукт 21'
         },
         {
-            'name': 'Стул премиального качества',
-            'desc': 'Просто попробуйте',
+            'name': 'Стул премиального качества', 
+            'desc': 'Просто попробуйте.',
             'image_src': 'product-31.jpg',
             'alt': 'продукт 31'
         },
     ]
-
-    content = {
-        'title': title,
-        'links_menu': links_menu,
-        'same_products': same_products
-    }
+    content = {'title': title, 'links_menu': links_menu, 'same_products': same_products}
     return render(request, 'mainapp/products.html', content)
-
+    
 
 def contact(request):
-    return render(request, 'mainapp/contact.html')
+    title = 'о нас'
+    visit_date = datetime.datetime.now()
+    locations = [
+        {
+            'city': 'Москва',
+            'phone': '+7-888-888-8888',
+            'email': 'info@geekshop.ru',
+            'address': 'В пределах МКАД',
+        },
+        {
+            'city': 'Екатеринбург',
+            'phone': '+7-777-777-7777',
+            'email': 'info_yekaterinburg@geekshop.ru',
+            'address': 'Близко к центру',
+        },
+        {
+            'city': 'Владивосток',
+            'phone': '+7-999-999-9999',
+            'email': 'info_vladivostok@geekshop.ru',
+            'address': 'Близко к океану',
+        },
+    ]
+    content = {'title': title, 'visit_date':visit_date, 'locations': locations}
+    return render(request, 'mainapp/contact.html', content)
+    
+    

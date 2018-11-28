f8c8c0749ca9e983ff7b70ebff46bbfcca17b5a9
jora <jora@email.ru>
2018-11-28 21:28:27 +0300

    Add my own auth model and user models's CRUD

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,15 +1,10 @@
-from django.conf import settings
-import mainapp.views as mainapp
 from django.urls import path
-from django.conf.urls.static import static
+
+import mainapp.views as mainapp
 
 app_name = 'mainapp'
-
 
 urlpatterns = [
     path('', mainapp.products, name='index'),
     path('<int:pk>/', mainapp.products, name='category'),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

7ddc81bc66d707e352bfe7a71624dad65a359638
jora <jora@email.ru>
2018-12-01 22:34:07 +0300

    Initial commit from src lesson_5

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -1,15 +1,16 @@
-from django.contrib import admin
-
-from django.urls import path, include
 from django.conf import settings
+from django.conf.urls import include
 from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path
 
 import mainapp.views as mainapp
 
 urlpatterns = [
     path('', mainapp.main, name='main'),
-    path('products/', include('mainapp.urls', namespace='mainapp.products')),
+    path('products/', include('mainapp.urls', namespace='products')),
     path('contact/', mainapp.contact, name='contact'),
+    path('auth/', include('authapp.urls', namespace='auth')),
     path('admin/', admin.site.urls),
 ]
 

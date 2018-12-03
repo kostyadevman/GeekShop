954350c2f0c1c732eb160a89f134a5835680a0ac
jora <jora@email.ru>
2018-12-03 22:07:41 +0300

    Initial commit from src lesson_6

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,93 +1,82 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 import datetime
 from .models import ProductCategory, Product
+from basketapp.models import Basket
+import random, os, json
+
+
+JSON_PATH = 'mainapp/json'
+
+
+def load_from_json(file_name):
+    with open(os.path.join(JSON_PATH, file_name + '.json'), 'r') as infile:
+        return json.load(infile)
 
 
 
+
+        
 def main(request):
-    title = 'главная'
-    # products = [
-    #     {
-    #         'name': 'Отличный стул',
-    #         'desc': 'Расположитесь комфортно.',
-    #         'image_src': 'product-1.jpg',
-    #         'image_href': '/product/1/',
-    #         'alt': 'продукт 1'
-    #     },
-    #     {
-    #         'name': 'Стул повышенного качества',
-    #         'desc': 'Не оторваться.',
-    #         'image_src': 'product-2.jpg',
-    #         'image_href': '/product/2/',
-    #         'alt': 'продукт 2'
-    #     },
-    # ]
-    products = Product.objects.all()[:4]
-    content = {'title': title, 'products': products}
+    title = 'главная'  
+    products = Product.objects.all()[:3]
+    
+    content = {
+        'title': title,
+        'products': products,
+        'basket': get_basket(request.user),
+    }
+    
     return render(request, 'mainapp/index.html', content)
     
 
 def products(request, pk=None):
-    print(pk)
-
     title = 'продукты'
-    # links_menu = [
-    #     {'href': 'products_all', 'name': 'все'},
-    #     {'href': 'products_home', 'name': 'дом'},
-    #     {'href': 'products_office', 'name': 'офис'},
-    #     {'href': 'products_modern', 'name': 'модерн'},
-    #     {'href': 'products_classic', 'name': 'классика'},
-    # ]
-    # same_products = [
-    #     {
-    #         'name': 'Отличный стул',
-    #         'desc': 'Не оторваться.',
-    #         'image_src': 'product-11.jpg',
-    #         'alt': 'продукт 11'
-    #     },
-    #     {
-    #         'name': 'Стул повышенного качества',
-    #         'desc': 'Комфортно.',
-    #         'image_src': 'product-21.jpg',
-    #         'alt': 'продукт 21'
-    #     },
-    #     {
-    #         'name': 'Стул премиального качества',
-    #         'desc': 'Просто попробуйте.',
-    #         'image_src': 'product-31.jpg',
-    #         'alt': 'продукт 31'
-    #     },
-    # ]
     links_menu = ProductCategory.objects.all()
-    same_products = Product.objects.all()
-    content = {'title': title, 'links_menu': links_menu, 'same_products': same_products}
+    basket = get_basket(request.user)
+           
+    if pk:
+        if pk is None:
+            category = {'name': 'все'}
+            products = Product.objects.all().order_by('price')
+        else:
+            category = get_object_or_404(ProductCategory, pk=pk)
+            products = Product.objects.filter(category__pk=pk).order_by('price')
+        
+        content = {
+            'title': title,
+            'links_menu': links_menu,
+            'category': category,
+            'products': products,
+            'basket': basket,
+        }
+        
+        return render(request, 'mainapp/products_list.html', content)
+    
+    same_products = get_same_products(hot_product)
+    
+    content = {
+        'title': title,
+        'links_menu': links_menu, 
+        'same_products': same_products,
+        'basket': basket,
+    }
+    
     return render(request, 'mainapp/products.html', content)
     
 
 def contact(request):
     title = 'о нас'
     visit_date = datetime.datetime.now()
-    locations = [
-        {
-            'city': 'Москва',
-            'phone': '+7-888-888-8888',
-            'email': 'info@geekshop.ru',
-            'address': 'В пределах МКАД',
-        },
-        {
-            'city': 'Екатеринбург',
-            'phone': '+7-777-777-7777',
-            'email': 'info_yekaterinburg@geekshop.ru',
-            'address': 'Близко к центру',
-        },
-        {
-            'city': 'Владивосток',
-            'phone': '+7-999-999-9999',
-            'email': 'info_vladivostok@geekshop.ru',
-            'address': 'Близко к океану',
-        },
-    ]
-    content = {'title': title, 'visit_date':visit_date, 'locations': locations}
+    
+    locations = load_from_json('contact__locations')
+    
+    content = {
+        'title': title,
+        'visit_date':visit_date, 
+        'locations': locations,
+        'basket': get_basket(request.user),
+    }
+    
     return render(request, 'mainapp/contact.html', content)
     
     
